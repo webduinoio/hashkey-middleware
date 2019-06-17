9948362ce37e0bf6591a0fadaebb966f11d57493
Duye Chen <k078264@gmail.com>
2019-06-17 11:18:22 +0800

    Init

---
+++ index.js
@@ -0,0 +1,44 @@
+'use strict';
+
+const Hashids = require('hashids');
+const NODE_ENV = process.env.NODE_ENV;
+const isProduction = (NODE_ENV === 'production');
+
+function hashKeyMiddlewareWrapper (options = {
+  salt: process.env.SALT || 'my salt',
+  hashkeyAliveTime: process.env.HASHKEY_ALIVE_TIME || 15,
+}) {
+  const hashSalt = options.salt;
+  const idHashids = new Hashids(hashSalt, 32, '0123456789abcdef');
+  // Hashkey alive time (minutes)
+  const HASHKEY_ALIVE_TIME = options.hashkeyAliveTime  * 60 * 1000;
+
+  return function hashKeyMiddleware(req, res, next) {
+    const hashKey = req.query.hashkey || req.query.access_token;
+    const decodedKey = idHashids.decode(hashKey);
+    const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+    const err = new Error('E_ACCESS_DENIED');
+          err.name = 'E_ACCESS_DENIED';
+          err.statusCode = 403;
+
+    if (decodedKey.length < 2) {
+      next(err);
+      return;
+    }
+
+    const now = Date.now();
+    const timeFromHash = decodedKey[1];
+
+    if (isProduction && (HASHKEY_ALIVE_TIME > 0) && (timeFromHash + (HASHKEY_ALIVE_TIME) < now)) {
+      next(err);
+      return;
+    }
+
+    res.locals.userId = decodedKey[0];
+    res.locals.hashKey = hashKey;
+    res.locals.ip = ip;
+    next();
+  };
+};
+
+module.exports = hashKeyMiddlewareWrapper;
